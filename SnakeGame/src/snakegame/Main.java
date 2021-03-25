c8ff048cd161c329dea56c8657a87de19193172c
cufon <cufon@cufon-81y3>
2021-03-25 18:19:10 +0300

    moved frame to a class field

--- SnakeGame/src/snakegame/Main.java
+++ SnakeGame/src/snakegame/Main.java
@@ -11,18 +11,23 @@ public class Main {
 	static Timer gameTimer;
 	private static SnakeScreen s;
 	private static SnakeGame game;
+	private static JFrame frame;
 	private static final int width = 500;
 	private static final int height = 500;
 	private static final int targetFPS = 60;
 	private static final int animDurationMS = 300;
 	public static void onGameOver(int score) {
-		
+		frame.getContentPane().removeAll();
+		gameTimer.stop();
+		game = null;
+		frame.getContentPane().add(new JLabel("YOU DIED"));
+		frame.setVisible(true);
 	}
 	public static void createGUI()
     {
-        JFrame frame = new JFrame("Snake");
+        frame = new JFrame("Snake");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(s);
+        frame.getContentPane().add(s);
         frame.setSize(new Dimension(width+10, height+30));
         frame.setResizable(false);
         frame.addKeyListener(new KeyEventHandler());
@@ -30,6 +35,7 @@ public class Main {
    }
 	public static void main(String[] args) {
 		game = new SnakeGame(width, height, 50, 50, animDurationMS / targetFPS);
+		game.registerGameOverHandler(Main::onGameOver);
 		s = game.getScreen();
 		gameTimer = new javax.swing.Timer(1000 / targetFPS, game::tick);
 		gameTimer.start();
